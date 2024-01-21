31c07f8d750e3355fedf4ca5f5c287551a2a3560
elanajaja <elanjaja01@gmail.com>
2024-01-21 12:38:51 -0800

    edited

--- pstr.c
+++ pstr.c
@@ -12,22 +12,18 @@ void pstr(stack_t **stack, unsigned int line_count)
 	stack_t *temp;
 	int count;
 
-	/* Suppress unused parameter warnings */
 	(void) *stack;
 	(void) line_count;
 
-	/* Set the current node to the head of the stack */
 	temp = file_data->head;
 	count = 0;
 
-	/* Check if the stack is empty */
 	if (temp == NULL)
 	{
 		printf("\n");
 	}
 	else
 	{
-		/* Iterate through the stack */
 		while (temp != NULL)
 		{
 			if (!(temp->n >= 65 && temp->n <= 90)
