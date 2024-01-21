31893138300028395728f6dfd8a48662e2ce0ce9
elanajaja <elanjaja01@gmail.com>
2024-01-21 12:58:38 -0800

    edited plall

--- pall.c
+++ pall.c
@@ -8,19 +8,14 @@
 void pall(stack_t **stack, unsigned int line_count)
 {
 	stack_t *current_node;
-
-	/* Avoid compiler warnings about unused parameters */
 	(void) stack;
 	(void) line_count;
 
-	/* Set the current_node pointer to the head of the stack */
 	current_node = file_data->head;
 
-	/* If the stack is empty, return */
 	if (current_node == NULL)
 		return;
 
-	/* Traverse the stack and print each element */
 	while (current_node != NULL)
 	{
 		printf("%d\n", current_node->n);
