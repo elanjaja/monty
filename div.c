c0003c687d8b313113002c584bf7c37494532b5a
elanajaja <elanjaja01@gmail.com>
2024-01-21 12:22:10 -0800

    edited

--- div.c
+++ div.c
@@ -17,7 +17,7 @@ void divide(stack_t **stack, unsigned int line_count)
 	temp = file_data->head;
 	if (temp == NULL || count_stack(file_data->head) < 2)
 	{
-		fprintf(stderr, "L%d: can't divide, stack too short\n", line_count);
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_count);
 		free_file_data();
 		exit(EXIT_FAILURE);
 	}
